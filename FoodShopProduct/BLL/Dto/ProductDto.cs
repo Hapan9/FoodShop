35260faa73e7fa1288810d792fb6086b0b589a64
Hapan9 <49860805+Hapan9@users.noreply.github.com>
2022-01-19 15:38:44 +0200

    v2.1
    Product description

--- FoodShopProduct/BLL/Dto/ProductDto.cs
+++ FoodShopProduct/BLL/Dto/ProductDto.cs
@@ -4,6 +4,7 @@
     {
         public string Name { get; set; }
         public float Price { get; set; }
+        public string Description { get; set; }
         public string Photo { get; set; }
     }
 }
--- FoodShopProduct/DAL/Models/Product.cs
+++ FoodShopProduct/DAL/Models/Product.cs
@@ -8,7 +8,7 @@ namespace DAL.Models
         public string Name { get; set; }
         public float Price { get; set; }
         public string Photo { get; set; }
-
+        public string Description { get; set; }
         public ProductInfo ProductInfo { get; set; }
     }
 }
