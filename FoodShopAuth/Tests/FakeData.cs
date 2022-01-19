4059ce11a08641b236515a5a108afe4c71b9f56f
Hapan9 <49860805+Hapan9@users.noreply.github.com>
2022-01-19 17:18:55 +0200

    v3

---
+++ FoodShopAuth/Tests/FakeData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoFixture;
+using DAL.Models;
+
+namespace Tests
+{
+    class FakeData
+    {
+        public List<User> Users { get; }
+
+        public Fixture Fixture { get; }
+
+        public FakeData()
+        {
+            Fixture = new Fixture();
+
+            Users = Fixture.CreateMany<User>(10)
+                .ToList();
+        }
+    }
+}
---
+++ FoodShopAuth/Tests/UnitTests/ControllerTests/UsersControllerTests.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BLL.Dto;
+using BLL.Interfaces;
+using Moq;
+using UI.Controllers;
+using Xunit;
+
+namespace Tests.UnitTests.ControllerTests
+{
+    public class UsersControllerTests
+    {
+        private FakeData _fakeData;
+        private readonly UsersController _usersController;
+        private readonly Mock<IUserService> _userServiceMock;
+
+        public UsersControllerTests()
+        {
+            _fakeData = new FakeData();
+
+            _userServiceMock = new Mock<IUserService>();
+            _usersController = new UsersController(_userServiceMock.Object);
+        }
+
+        [Fact]
+        public async Task Get_ServiceInvoke()
+        {
+            //Act
+            await _usersController.Get(It.IsAny<Guid>());
+
+            //Assert
+            _userServiceMock.Verify(s => s.Get(It.IsAny<Guid>()));
+        }
+
+        [Fact]
+        public async Task GetAll_ServiceInvoke()
+        {
+            //Act
+            await _usersController.GetAll();
+
+            //Assert
+            _userServiceMock.Verify(s => s.GetAll());
+        }
+
+        [Fact]
+        public async Task Create_ServiceInvoke()
+        {
+            //Act
+            await _usersController.Create(It.IsAny<UserDto>());
+
+            //Assert
+            _userServiceMock.Verify(s => s.Create(It.IsAny<UserDto>()));
+        }
+
+        [Fact]
+        public async Task Update_ServiceInvoke()
+        {
+            //Act
+            await _usersController.Update(It.IsAny<Guid>(), It.IsAny<UserDto>());
+
+            //Assert
+            _userServiceMock.Verify(s => s.Update(It.IsAny<Guid>(), It.IsAny<UserDto>()));
+        }
+
+        [Fact]
+        public async Task Delete_ServiceInvoke()
+        {
+            //Act
+            await _usersController.Delete(It.IsAny<Guid>());
+
+            //Assert
+            _userServiceMock.Verify(s => s.Delete(It.IsAny<Guid>()));
+        }
+    }
+}
